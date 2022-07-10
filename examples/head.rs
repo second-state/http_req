e866ae87a389208b28bd4ca4cab23c7b4fab0511
2021 <458761603@qq.com>
2022-07-10 16:41:01 +0000

    [Lib] update wasmedge_wasi_socket

--- examples/head.rs
+++ examples/head.rs
@@ -1,7 +1,7 @@
 use wasmedge_http_req::request;
 
 fn main() {
-    let res = request::head("https://www.rust-lang.org/learn").unwrap();
+    let res = request::head("http://httpbin.org/head").unwrap();
 
     println!("Status: {} {}", res.status_code(), res.reason());
     println!("{:?}", res.headers());
