631fc710718bfeeeee15e11867eecd190803a6bf
zhouzhou <juliazhou2019@outlook.com>
2022-08-23 08:58:31 +0800

    [Example] add example for wasmedge_http_req
    Signed-off-by: zhouzhou <juliazhou2019@outlook.com>

---
+++ examples/add_headers.rs
@@ -0,0 +1,30 @@
+use std::convert::TryFrom;
+use wasmedge_http_req::{request::{Request, HttpVersion}, response::Headers, uri::Uri};
+
+fn main() {
+    let mut writer = Vec::new();
+    let uri = Uri::try_from("http://httpbin.org/get").unwrap();
+    // let uri = Uri::try_from("https://httpbin.org/get").unwrap(); // uncomment the line for https request
+
+    // add headers to the request
+    let mut headers = Headers::new();
+    headers.insert("Accept-Charset", "utf-8");
+    headers.insert("Accept-Language", "en-US");
+    headers.insert("Host", "rust-lang.org");
+    headers.insert("Connection", "Close");
+    
+    let mut response = Request::new(&uri)
+        .headers(headers)
+        .send(&mut writer)
+        .unwrap();
+
+    println!("{}", String::from_utf8_lossy(&writer));
+
+    // set version
+    response = Request::new(&uri)
+        .version(HttpVersion::Http10)
+        .send(&mut writer)
+        .unwrap();
+    
+    println!("{}", String::from_utf8_lossy(&writer));
+}
