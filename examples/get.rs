5bff8839d0bb8c898029ea9e0be90406ce5a912e
Michael Yuan <michael@michaelyuan.com>
2022-09-30 02:24:48 +0800

    Update get.rs
    Signed-off-by: Michael Yuan <michael@michaelyuan.com>

--- examples/get.rs
+++ examples/get.rs
@@ -2,7 +2,7 @@ use http_req_wasi::request;
 
 fn main() {
     let mut writer = Vec::new(); //container for body of a response
-    let res = request::get("http://httpbin.org/get?msg=WasmEdge", &mut writer).unwrap();
+    let res = request::get("http://eu.httpbin.org/get?msg=WasmEdge", &mut writer).unwrap();
 
     println!("Status: {} {}", res.status_code(), res.reason());
     println!("Headers {}", res.headers());
