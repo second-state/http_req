d0ef4533db61be3966660a01085a97447474d45b
Michael Yuan <michael@michaelyuan.com>
2023-07-05 01:29:47 -0500

    Update get_https.rs

--- examples/get_https.rs
+++ examples/get_https.rs
@@ -2,7 +2,7 @@ use http_req::request;
 
 fn main() {
     let mut writer = Vec::new(); //container for body of a response
-    let res = request::get("https://httpbin.org/get?msg=WasmEdge", &mut writer).unwrap();
+    let res = request::get("https://cdn.discordapp.com/attachments/1125835659064643655/1126012053631545454/IMG_6971.jpg", &mut writer).unwrap();
 
     println!("Status: {} {}", res.status_code(), res.reason());
     println!("Headers {}", res.headers());
